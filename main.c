1c95bce1e768a6f04543d177a5756ff5953ec6c1
daehee1018 <handh011018@gmail.com>
2023-11-06 20:24:02 +0900

    no message

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 int main(void){
-    printf("야옹스 프로젝트\n");
+    int start_num;
+   printf("----------Cat Snack Collection Game----------\n");
+    printf("|1. 가위바위보 게임                         |\n");
+    printf("|2. 블랙잭 게임                             |\n");
+    printf("|3. 최종 결과                               |\n");
+    printf("---------------------------------------------\n");
+    
+    printf("시작하려면 '1'을 누르세요!");
+    scanf("%d", &start_num);
 
-    return 0;
+    
+  return 0;
 }
